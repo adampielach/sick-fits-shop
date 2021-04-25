fb92580efb1d7dd40e483baf0c2d6369abbc15f4
Adam Pielach <adampielach@gmail.com>
2021-04-25 19:27:21 +0200

    * added main style for the app * added nprogress for the app * added keystone basic config setup * connected mongodb to the app (keystone) * created User schema

---
+++ backend/keystone.ts
@@ -0,0 +1,31 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long are we signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: add data seeding here
+  },
+  lists: createSchema({
+    // schema items go in here
+  }),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
